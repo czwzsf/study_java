8054e3ca6483c86036eddeaaf6f40a8b047ef494
zhewei chen <38755662+czwzsf@users.noreply.github.com>
2022-05-23 00:32:34 +0800

    version2

--- src/day_3/Stars.java
+++ src/day_3/Stars.java
@@ -6,7 +6,7 @@ public class Stars {
         //打印空心金字塔
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.println("*");
+                System.out.println("******!");
             }
         }
     }
