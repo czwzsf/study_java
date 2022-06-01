001ae902977e49739753d84320f6365d20338a76
zhewei chen <1241770441@qq.com>
2022-06-01 13:30:27 +0800

    6/1 for语句

---
+++ src/day_4/forDetail.java
@@ -0,0 +1,9 @@
+package day_4;
+
+public class forDetail {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 12; i++){
+            System.out.println("!!!!!!!!");
+        }
+    }
+}
