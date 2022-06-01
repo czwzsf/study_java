9b7578a47e38949bb65c3308c7b5da4397dcc69a
zhewei chen <1241770441@qq.com>
2022-06-01 09:50:42 +0800

    6/1 if语句

--- src/day_4/if01.java
+++ src/day_4/if01.java
@@ -1,7 +1,18 @@
 package day_4;
 
+import java.util.Scanner;
+
 public class if01 {
     public static void main(String[] args) {
-
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("请输入信用分：");
+        int grade = myScanner.nextInt();
+        if (grade == 100) {
+            System.out.println("信用极好");
+        } else if (grade > 80 && grade <= 99) {
+            System.out.println("信用优秀");
+        } else {
+            System.out.println("信用不及格");
+        }
     }
 }
